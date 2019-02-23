7f5e3ff476351777655e3e71a111316209d41df9
cgolobic <cgolobic@gmail.com>
2019-02-23 16:02:16 -0700

    feat: Angular Universal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    AngularUniversalModule.forRoot({
+      viewsPath: join(process.cwd(), 'client/dist/app-client'),
+      bundle: require('./../client/dist/app-server/main.js')
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

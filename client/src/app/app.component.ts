028b4e3d572d69d5264ce4b0d50c50306bfdf39f
cgolobic <cgolobic@gmail.com>
2019-02-23 16:02:16 -0700

    feat: Angular Universal

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IGreeting } from '../../../shared/interfaces/greeting';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-client';
+  greeting = '';
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit() {
+    this._http.get('http://localhost:3000/api/hello').subscribe((res: IGreeting) => this.greeting = res.greeting);
+  }
+
 }
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IGreeting } from 'shared/interfaces/greeting';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('api/hello')
+  getHello(): IGreeting {
+    return { greeting: this.appService.getHello() };
   }
 }

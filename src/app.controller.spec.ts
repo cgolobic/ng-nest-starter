ce8dc60f70427409da25feac6dad71ba422b073e
Connor Golobich <connor.golobich@drivetime.com>
2019-02-25 08:13:34 -0700

    feat: use jasmine for server unit tests, update client unit tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "{ greeting: \'Hello World!\' }"', () => {
+      expect(appController.getHello()).toEqual({ greeting: 'Hello World!' });
     });
   });
 });
